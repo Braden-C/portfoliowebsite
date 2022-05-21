1385a9a3c56dbe31611c2a9eee2a277fe0dde2ef
Braden C <bradencchapman@yahoo.com>
2022-05-20 20:25:37 -0600

    add technologies list

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,6 +1,28 @@
 import React from "react";
 import Header from "./Header";
+
+
+const technologies = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React.JS",
+    "Git",
+    "Npm",
+];
+
+
+
 const AboutMe = (props) => {
+    const renderTechList = () => {
+        return technologies.map((item, index) => {
+            return <li className="technology-item" key={index}>
+                <i className="fas fa-terminal"></i>
+                <span>{item}</span>
+            </li>
+        });
+    };
+
     return (
         <div className="about-me-container" id="About">
             <div className="container-md">
@@ -9,6 +31,10 @@ const AboutMe = (props) => {
                     <strong> Hello, i am a 27 year old Front-End developer based in central Colorado.</strong>
                     <span> I became interested in software/Web development at a very young age, My passion for technology and gaming has only grown over the years and lead me to pursue a career in tech.</span>
                 </p>
+                <span className="sub-header">Technologies: </span>
+                <ul className="technology-list">
+                    {renderTechList()}
+                </ul>
             </div>
             <div className="img-container">
                 <img src=".\websitephoto.jpg" className="img-fluid" alt="self portrait"></img>
