a38d6e637f1a6761a14be35ff0685f6cdd1ac5b2
Braden C <bradencchapman@yahoo.com>
2022-05-20 18:13:08 -0600

    fix spelling error and hero scaling

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -7,7 +7,7 @@ const AboutMe = (props) => {
                 <Header label="About Me" />
                 <p className="Lead">
                     <strong> Hello, i am a 27 year old Front-End developer based in central Colorado.</strong>
-                    <span> I became interested in software/Web development at a very young age, My passion for technology and gaming has only grown over the years and lead me to persue a career in tech.</span>
+                    <span> I became interested in software/Web development at a very young age, My passion for technology and gaming has only grown over the years and lead me to pursue a career in tech.</span>
                 </p>
             </div>
             <div className="img-container">
